acd616348211822459caf971bf5ab194de8407b1
Frank_Gary <blackrebelradio1992@gmail.com>
2023-11-30 08:47:13 -0600

    in itial

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Container from "react-boootstrap-Container"
+
+function App() {
+ return (
+    <Container>
+      <Row></Row>
+    </Container>
+ )
+}
+
+export default App
